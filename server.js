6847e082abb4d819ddee46153eb131dbf7b11612
Elin Ronda <elro2506@student.miun.se>
2026-05-08 12:48:46 +0200

    Lagt till Cors-middleware

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 app.use(bodyParser.json());
 
 //Routes
